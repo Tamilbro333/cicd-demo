593d9a83f47686f019b514f27e5a4d48cc684f96
Tamilbro333 <ushagowri1982@gmail.com>
2026-03-23 18:00:48 +0530

    Update version to 1.0.4 for demo

--- test_app.py
+++ test_app.py
@@ -38,6 +38,7 @@ def test_docker_endpoint(client):
     data = json.loads(response.data)
     assert 'logs' in data
 #comment line
+
 def test_health_endpoint(client):
     response = client.get('/health')
     data = json.loads(response.data)
